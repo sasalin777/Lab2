eea02f4fcbb8048654626eb34a2fcb3f034d63eb
sasalin777 <candysasame@gmail.com>
2021-10-18 12:41:20 -0700

    update

--- src/Canada.java
+++ src/Canada.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Canada {
+public class Canada {
+
+
+     public static String[] Province = {"British Columbia", "Alberta", "Manitoba",
+            "Saskatchewan", "Northwest Territories", "Nova Scotia",
+            "Nunavut", "Ontario", "Prince Edward Island", "Quebec"};
+
+    public static String[] capital = {"Victoria", "Edmonton", "Winnipeg",
+            "Regina", "Yellowknife", "Halifax",
+            "Iqaluit", "Toronto", "Charlottetown", "Quebec City"};
+
+    public static long[] population = {4648055, 4067175, 1278365, 1098352, 41786, 923598, 35944, 13448494, 142907, 8164361};
+
+
+    public Canada() { this(Province, capital, population );}
+
+    public Canada(String[] province, String[] capital, long[] population) {
+        Canada.Province=Province;
+        Canada.capital=capital;
+        Canada.population =population;
+
+    }
+
+    public static String displayAllProvinces() {
+        int i=0;
+        int l=Province.length;
+        for (i=0;i<=l-1;i++) {
+
+            System.out.printf("\nThe capital of %s (population. %d) is %s.",Province[i],population[i],capital[i]);
+        }
+                return "";
+    }
+    public boolean getNumOfProvincesBetween(int min, int max){
+       long lmin=min*1000000;
+       long lmax=max*1000000;
+       for (int j=0;j<= Province.length-1;j++){
+            if ((Canada.population[j] >= lmin) && (Canada.population[j] <= lmax)) {
+                System.out.printf("\nThe capital of %s (population. %d) is %s.",Province[j],population[j],capital[j]);
+
+            }
+        }
+
+
+        return false;
+    }
+
+
 }
--- src/Province.java
+++ src/Province.java
@@ -1,2 +1,124 @@
-package PACKAGE_NAME;public class Province {
+import java.util.Arrays;
+
+
+public class Province {
+
+    private String name;
+    private String capital;
+    private long population;
+
+    public static final String DEFAULT_PROVINCE="British Columbia";
+    public static final String DEFAULT_CAPITAL="Victoria";
+    public static final long DEFAULT_POPULATION=4648055;
+
+    //contractor
+
+    public Province() { this(DEFAULT_PROVINCE, DEFAULT_CAPITAL, DEFAULT_POPULATION );}
+    public Province(String name,String capital, long population)
+    {
+        if ((isValidProvince(getName())) && (isValidCapitalCity(getCapital())) &&(isValidPopulation(getPopulation())))
+        {
+        //checkName(name);
+        //checkCapital(capital);
+        //checkPopulation(population);
+        setName(name);
+        setCapital(capital);
+        setPopulation(population);}
+        else{
+        this.population = DEFAULT_POPULATION; // 4,648,055
+        this.name = DEFAULT_PROVINCE; // “British Columbia”
+        this.capital = DEFAULT_CAPITAL; // “Victoria”
+
+    }
+    }
+
+
+
+    //getdata
+
+
+
+
+
+    //check isValidProvince
+    boolean isValidProvince(String getName){
+        String provinceArray[]={"British Columbia","Yukon","New Brunswick",
+                "Newfoundland and Labrador","Northwest Territories","Nova Scotia",
+                "Nunavut","Ontario","Prince Edward Island","Quebec"};
+
+        int checkProvince=Arrays.asList(provinceArray).indexOf(getName);
+
+        return (checkProvince != -1  );
+    }
+    //check isValidCapitalCity
+
+    boolean isValidCapitalCity(String getCapital){
+        String CapitalCityArray[]={"Victoria","Whitehorse","Fredericton",
+                "St. John's","Yellowknife","Halifax",
+                "Iqaluit","Toronto","Charlottetown","Québec City"};
+
+        int checkProvince=Arrays.asList(CapitalCityArray).indexOf(getCapital);
+
+        return (checkProvince != -1  );
+
+    }
+
+
+    boolean isValidPopulation(long population){
+
+        return ((getPopulation()>=30000) && (getPopulation()<=15000000) );
+
+    }
+
+
+
+
+
+
+    public String getDetails(){
+
+
+        return String.format("The capital of %s (population. %d) is %s.",getName(),getPopulation(),getCapital());
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public final void setName(String name) {
+        //checkName(name);
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public final void setCapital(String capital) {
+        //checkCapital(capital);
+        this.capital = capital;
+    }
+
+    public long getPopulation() { return population; }
+
+    public final void setPopulation(long population) {
+        //checkPopulation(population);
+        this.population = population;
+    }
+
+
+
 }
